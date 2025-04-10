efa02276315c144fd0535e02b5d05bfc2b83b8af
Dynamiteboem2 <danielvangrol2006@gmail.com>
2025-04-10 10:46:25 +0200

    Route werkt naar reservation

---
+++ app/Http/Controllers/ReservationController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Reservation;
+use Illuminate\Http\Request;
+
+class ReservationController extends Controller
+{
+    public function index()
+    {
+        $reservations = Reservation::all();
+        return view('reservations.index', compact('reservations'));
+    }
+}
---
+++ routes/daniel.php
@@ -0,0 +1,5 @@
+<?php
+use App\Http\Controllers\ReservationController;
+use Illuminate\Support\Facades\Route;
+
+Route::resource('reservations', ReservationController::class);
