d0e998fe26c7d859d4b0486a46ad232094d0aa3b
mahdi2662 <madialkhalidi83@gmail.com>
2025-04-10 14:35:03 +0200

    ben klaat met sp

--- database/sp/OrdersSP/spGetAllOrders.sql
+++ database/sp/OrdersSP/spGetAllOrders.sql
@@ -1,18 +1,16 @@
 CREATE PROCEDURE spGetAllOrders()
 BEGIN
     SELECT 
-        Orders.id,
-        Orders.user_id,
-        Orders.bowling_alley_id,
-        Orders.Total_price,
-        Orders.product,
-        Orders.isactive,
-        Orders.note,
-        Orders.created_at,
-        Orders.updated_at,
-        users.name AS user_name
+        orders.id,
+        orders.bowling_alleyid,
+        orders.product,
+        orders.status,
+        orders.price,
+        orders.total_price,
+        orders.isactive,
+        orders.note,
+        orders.created_at,
+        orders.updated_at
     FROM 
-        reservations
-    INNER JOIN 
-        users ON reservations.user_id = users.id;
+        orders;
 END;
