ff03321f9ff63a456d539a2f6e2d3d93965cc8a1
mahdi2662 <madialkhalidi83@gmail.com>
2025-04-10 14:34:49 +0200

    ben klaar met alle sp

--- database/migrations/2026_01_15_000003_create_order_procedures.php
+++ database/migrations/2026_01_15_000003_create_order_procedures.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Log;
 
 return new class extends Migration
 {
@@ -13,10 +14,41 @@ return new class extends Migration
     {
         // Drop existing procedure if it exists
         DB::unprepared('DROP PROCEDURE IF EXISTS spGetAllOrders');
+        DB::unprepared('DROP PROCEDURE IF EXISTS spDeleteOrder');
+        DB::unprepared('DROP PROCEDURE IF EXISTS spUpdateOrder');
+        DB::unprepared('DROP PROCEDURE IF EXISTS spCreateOrder');
 
         // Create spGetAllOrders procedure
         $pathGetAllOrders = database_path('sp/OrdersSP/spGetAllOrders.sql');
-        DB::unprepared(File::get($pathGetAllOrders));
+        if (File::exists($pathGetAllOrders)) {
+            DB::unprepared(File::get($pathGetAllOrders));
+        } else {
+            Log::warning("File not found: $pathGetAllOrders");
+        }
+
+        // Create spDeleteOrder procedure
+        $pathDeleteOrder = database_path('sp/OrdersSP/spDeleteOrder.sql');
+        if (File::exists($pathDeleteOrder)) {
+            DB::unprepared(File::get($pathDeleteOrder));
+        } else {
+            Log::warning("File not found: $pathDeleteOrder");
+        }
+
+        // Create spUpdateOrder procedure
+        $pathUpdateOrder = database_path('sp/OrdersSP/spUpdateOrder.sql');
+        if (File::exists($pathUpdateOrder)) {
+            DB::unprepared(File::get($pathUpdateOrder));
+        } else {
+            Log::warning("File not found: $pathUpdateOrder");
+        }
+
+        // Create spCreateOrder procedure
+        $pathCreateOrder = database_path('sp/OrdersSP/spCreateOrder.sql');
+        if (File::exists($pathCreateOrder)) {
+            DB::unprepared(File::get($pathCreateOrder));
+        } else {
+            Log::warning("File not found: $pathCreateOrder");
+        }
     }
 
     /**
@@ -26,5 +58,8 @@ return new class extends Migration
     {
         // Drop the procedure
         DB::unprepared('DROP PROCEDURE IF EXISTS spGetAllOrders');
+        DB::unprepared('DROP PROCEDURE IF EXISTS spDeleteOrder');
+        DB::unprepared('DROP PROCEDURE IF EXISTS spUpdateOrder');
+        DB::unprepared('DROP PROCEDURE IF EXISTS spCreateOrder');
     }
 };
